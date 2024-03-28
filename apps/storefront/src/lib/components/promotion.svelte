892c8ba3ad1bf03fa2e969572725c4c060a11fae
reasat-rafio <rafioar96@gmail.com>
2024-03-28 20:04:07 +0600

    :sparkles: Promotion bar added

--- apps/storefront/src/lib/components/promotion.svelte
+++ apps/storefront/src/lib/components/promotion.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import type { Promotion } from '$lib/types/site.types';
   import { PortableText } from '@portabletext/svelte';
+  import { X } from 'lucide-svelte';
   import { onMount } from 'svelte';
+  import { fade, slide } from 'svelte/transition';
 
   export let promotions: Promotion[];
 
@@ -20,9 +22,21 @@
   });
 </script>
 
-<div>
-  <div
-    class="bg-primary text-primary-foreground container py-1 text-center text-sm">
-    <PortableText components={{}} value={promotions[activeIndex].title} />
+{#if !hide}
+  <div transition:slide>
+    <div
+      class="bg-primary text-primary-foreground text-xxs relative py-2 text-center text-xs font-medium md:text-xs">
+      {#key promotions[activeIndex].title}
+        <div class="container" in:fade>
+          <PortableText components={{}} value={promotions[activeIndex].title} />
+        </div>
+      {/key}
+
+      <button
+        on:click={() => (hide = true)}
+        class="absolute right-2 top-1/2 -translate-y-1/2">
+        <X size={18} color="white" />
+      </button>
+    </div>
   </div>
-</div>
+{/if}
